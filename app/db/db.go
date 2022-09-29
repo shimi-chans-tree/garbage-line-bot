804eaa09925950d40b802ef903205f97165137bc
koji <koji@kojinoMacBook-Pro.local>
2022-09-29 20:08:02 +0900

    DB接続周りの実装

---
+++ app/db/db.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+/*
+* DBの接続設定を実施
+ */
+func Init() *gorm.DB {
+	// .envを読み込む
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	// MySQLへの接続情報を定義
+	dsn := os.Getenv(("MYSQL_USER")) + ":" + os.Getenv(("MYSQL_PASSWORD")) + "@tcp(" + os.Getenv(("MYSQL_HOST")) + ":" + os.Getenv(("MYSQL_PORT")) + ")/" + os.Getenv(("MYSQL_DATABASE")) + "?charset=utf8mb4&parseTime=True&loc=Local"
+	fmt.Println(dsn)
+	// DBインスタンスを生成
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+/*
+* DBの接続情報を取得
+ */
+// func GetDB() *gorm.DB {
+// 	return DB
+// }
+
+/*
+* DBを閉じる
+ */
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err = sqlDB.Close(); err != nil {
+		panic(err)
+	}
+}
