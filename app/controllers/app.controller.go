a92737876b1e17a0894e62b3bf66fa5f1ebd2c34
koji <koji@kojinoMacBook-Pro.local>
2022-09-30 19:24:52 +0900

    動作確認用controller,routerの実装

---
+++ app/controllers/app.controller.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type AppController interface {
+	RootPage(w http.ResponseWriter, r *http.Request)
+}
+
+type appController struct{}
+
+func NewAppController() AppController {
+	return &appController{}
+}
+
+/*
+* ルートAPI
+ */
+func (apc *appController) RootPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Go Api Serverss")
+}
